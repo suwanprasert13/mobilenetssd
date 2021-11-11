138f83b658f27130e27d2246aca10b67c3fd7fb6
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-11 13:30:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def process_file(path, filename):
 def detect_object(path, filename):    
     CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
         "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-        "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+        "dog", "horse", "motorbike", "person", "pottedplant",  "sheep",
         "sofa", "train", "tvmonitor"]
     COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
     prototxt="ssd/MobileNetSSD_deploy.prototxt.txt"
