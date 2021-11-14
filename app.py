8ea79436d43be5cde5e13c91ebfa272f01249611
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-15 01:02:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def event_handle(event,hosts):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        replyObj = TextSendMessage(text=hosts['Host'])
+        replyObj = TextSendMessage(text=hosts)
         line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
