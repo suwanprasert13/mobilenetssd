8b7e2fc48211ecb012ae06e545d4c234a2d2d21c
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-15 01:09:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,7 @@ def event_handle(event,hosts):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
+        hosts['Host'] = "bots.dialogflow.com"
         replyObj = TextSendMessage(text=str(hosts))
         line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
