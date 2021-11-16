c18acec8757e3ab9b775dedd13199540d2c62fa7
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-16 14:49:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,10 @@ def event_handle(event):
         msg = str(event["message"]["text"])
         if msg == "สวัสดี" :
             replyObj = TextSendMessage(text="เออ...ดีด้วย")
+        elif msg == "กินข้าวไหม" :
+            replyObj = TextSendMessage(text="กินซิ หิวมาก")
+        elif msg == "ไปเที่ยวกันไหม" :
+            replyObj = TextSendMessage(text="ไปดิ")
         else :
             replyObj = TextSendMessage(text="ไม่รู้ไม่ชี้")
         line_bot_api.reply_message(rtoken, replyObj)
