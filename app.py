ffdb9c72fd46a5f980b655e96c07b9170ff436d3
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-18 21:39:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -169,9 +169,8 @@ def event_handle(event):
             crl= pycurl.Curl()
             crl.setopt( crl.URL, "https://bots.dialogflow.com/line/k--jomf/webhook")
             crl.setopt( crl.POST, 1)
-            '''
             crl.setopt( crl.BINARYTRANSFER, true)
-            crl.setopt( crl.POSTFIELDS, decoded)
+            crl.setopt( crl.POSTFIELDS, json_headers)
             crl.setopt( crl.HTTPHEADER, json_headers)
             crl.setopt( crl.SSL_VERIFYHOST, 2)
             crl.setopt( crl.SSL_VERIFYPEER, 1)
@@ -179,7 +178,7 @@ def event_handle(event):
             crl.setopt( crl.RETURNTRANSFER, 1)
             #crl.perform()
             #crl.close()
-            '''
+            
             replyObj = TextSendMessage(text=json_headers)
             line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
