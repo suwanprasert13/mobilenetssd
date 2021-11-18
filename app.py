e6fe8dd142167ac62624cd1fd46313b0fdd30b36
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-18 07:10:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def event_handle(event):
     if msgType == "text":
         headers = request.headers
         headers = {'X-Line-Signature':headers['X-Line-Signature'],'Host':'bots.dialogflow.com'}
-        headers = json.dumps(headers)
+        json_headers = json.dumps(headers)
 #        json_headers = json.loads(headers)
         
         msg = str(event["message"]["text"])
