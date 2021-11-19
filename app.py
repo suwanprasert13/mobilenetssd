f1c9d943fea9d43909099d20af463d8ceaa01950
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-20 04:41:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -160,11 +160,11 @@ def event_handle(event,decoded):
             line_bot_api.reply_message(rtoken, replyObj)
         else :
             headers = request.headers
-            #json_headers = json.dumps({k:v for k, v in headers.items()})
             for k, v in headers.items():
                 if k=='Host':
                     v = 'dialogflow.com'
-                    json_headers = v
+                    #json_headers = v
+            json_headers = json.dumps({k:v for k, v in headers.items()})
             '''
             json_line = request.get_json(force=False,cache=False)
             json_line = json.dumps(json_line)
