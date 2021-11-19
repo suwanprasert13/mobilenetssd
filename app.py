581fa2c856103a9d30284d1f96a8a002ea2a30c7
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-20 01:58:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ def event_handle(event,decoded):
             crl.perform()
             crl.close()
             
-            replyObj = TextSendMessage(text=decoded)
+            replyObj = TextSendMessage(text=str(decoded))
             line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
