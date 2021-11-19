89c8610cac64c7e9e5a493ba9d85554abb3cec64
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-20 04:01:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ def event_handle(event,decoded):
             crl.perform()
             crl.close()
             
-            replyObj = TextSendMessage(text=str(request.get_data(as_text=True)))
+            replyObj = TextSendMessage(text=str(request.get_json()))
             line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
