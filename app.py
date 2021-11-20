729fd6e15e906981dd51c5fc8c3d6a534e29e20f
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-20 21:26:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def event_handle(event,decoded):
             decoded = json.loads(json_line)
             '''
             url = "https://bots.dialogflow.com/line/k--jomf/webhook"
-            requests.post(url,data=decoded, headers=json_headers)
+            r=requests.post(url,data=decoded, headers=json_headers)
             crl= pycurl.Curl()
             crl.setopt( pycurl.URL, "https://bots.dialogflow.com/line/k--jomf/webhook")
             crl.setopt( pycurl.POST, 1)
