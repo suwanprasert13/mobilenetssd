7cae9454d175c8233171ceb16d0758ef289fd430
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-22 00:37:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -162,6 +162,11 @@ def event_handle(event,json_line):
             response = requests.get(url)
             replyObj = TextSendMessage(text=str(response))
             line_bot_api.reply_message(rtoken, replyObj)
+        elif msg == "covid" :
+            url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
+            response = requests.get(url)
+            replyObj = TextSendMessage(text=str(response))
+            line_bot_api.reply_message(rtoken, replyObj)
         else :
             headers = request.headers
             json_headers = ({k:v for k, v in headers.items()})
