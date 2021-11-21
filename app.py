9abf8c823d432b3a93383b71cf3c1b589421d0a3
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-22 00:57:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -160,11 +160,13 @@ def event_handle(event,json_line):
         elif msg == "พยากรณ์อากาศ" :
             url = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q={Trat}&appid=d7d8657acc2cba4726e59bcd6394e2ba"
             response = requests.get(url)
+            response = response.json()
             replyObj = TextSendMessage(text=str(response))
             line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "covid" :
             url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
             response = requests.get(url)
+            response = response.json()
             replyObj = TextSendMessage(text=str(response))
             line_bot_api.reply_message(rtoken, replyObj)
         else :
