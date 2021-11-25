8066267b5b080e49f13fbc8fbdd1ddc4fe6e0f31
suwanprasert13 <87840231+suwanprasert13@users.noreply.github.com>
2021-11-25 10:56:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def callback():
 def reply(intent,text,reply_token,id,disname):
     text_message = TextSendMessage(text="ทดสอบ")
     line_bot_api.reply_message(reply_token,text_message)
-
+    
 def event_handle(event,json_line):
     try:
         userId = event['source']['userId']
@@ -158,17 +158,11 @@ def event_handle(event,json_line):
         elif msg == "ไปเที่ยวกันไหม":
             replyObj = TextSendMessage(text="ไปดิ")
             line_bot_api.reply_message(rtoken,replyObj)
-        elif msg == "covid" :
-            url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
-            response = requests.get(url)
-            response = response.json()
-            replyObj = TextSendMessage(text=str(response))
-            line_bot_api.reply_message(rtoken, replyObj)
         else :
             headers = request.headers
             json_headers = ({k:v for k, v in headers.items()})
             json_headers.update({'Host':'bots.dialogflow.com'})
-            url = "https://bots.dialogflow.com/line/931b7ef7-3948-402e-a49c-76786e302ebc/webhook"
+            url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/931b7ef7-3948-402e-a49c-76786e302ebc"
             requests.post(url,data=json_line, headers=json_headers)
     elif msgType == "image":
         try:
